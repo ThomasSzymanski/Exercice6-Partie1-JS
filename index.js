8d37d1dd77ec5ee70fcd04d1d553aebd22d4c516
Thomas Szymanski <thomas.szymanski15@gmail.com>
2017-10-26 10:01:07 +0200

    Exercice 6 - Partie 1 - JAVASCRIPT

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function diviser(){
 	var first = document.getElementById('premier_nombre').value;
     var second = document.getElementById('deuxieme_nombre').value;
-    var division = (first % second)
-    alert(division)
+      if (isNaN(first) == false && isNaN(second) == false && second != 0) {
+    var result = (first) % (second);
+    alert('Le résultat est : ' + result);
+  } else {
+    alert('Veuillez saisir un nombre');
+  }
 }
